2c2b4a7d79b4f415a342a5f1c36fe4844df56a00
michaelmotes <michaelmotes@email.com>
2013-09-18 10:28:39 -0500

    moving remote directory

--- PKCS12toPEM.h
+++
@@ -1,1 +0,0 @@
-extern "C"  __declspec(dllexport) int _cdecl PFXtoPEM(char* PFXinfile, char* password, char* PEMoutfile);
--- stdafx.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// PKCS12toPEM.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
